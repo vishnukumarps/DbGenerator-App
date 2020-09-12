c8bb1cb594ddee5e2faa255ad6e89ab5db781ed8
vishnukumarps <vishnukumar29518@gmail.com>
2020-09-12 09:17:34 +0530

    Add project files.

---
+++ HelperUtility.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Net.Mail;
+using System.Web;
+
+namespace TestAppLibrary
+{
+    public  class HelperUtility
+    {
+        private const string connectionString = "Server=localhost\\SQLEXPRESS;Database=ZTestDb;Trusted_Connection=True";
+
+        public bool Status { get; set; }
+        public string ErrorMessage { get; set; }
+
+
+        public bool CreateDb()
+        {
+            return false;
+        }
+
+        public List<string> GetAllTableNames()
+        {
+            List<string> tableNames = new List<string>();
+            var Query = " SELECT  name FROM sys.Tables";
+            try
+            {
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+                SqlCommand sqlCommand = new SqlCommand(Query, connection);
+                DataReader = sqlCommand.ExecuteReader();
+                while(DataReader.Read())
+                {
+                    tableNames.Add(DataReader.GetValue(0).ToString());
+                }
+                connection.Close();
+                return tableNames;
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+        public Dictionary<string,string> GetTableSchema(string tableName)
+        {
+
+
+            Dictionary<string, string> tableSchemea = new Dictionary<string, string>();
+
+            string Query = "SELECT  COLUMN_NAME,DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = N'"+tableName+"'";
+            try
+            {
+                try
+                {
+                    SqlConnection connection = new SqlConnection(connectionString);
+                    connection.Open();
+                    SqlCommand sqlCommand = new SqlCommand(Query, connection);
+                    DataReader = sqlCommand.ExecuteReader();
+                    while (DataReader.Read())
+                    {
+                        tableSchemea.Add(DataReader.GetValue(0).ToString(),DataReader.GetValue(1).ToString());
+                    }
+                    connection.Close();
+                    return tableSchemea;
+
+                }
+                catch (Exception ex)
+                {
+
+                    ErrorMessage = ex.Message;
+                    Status = true;
+                }
+            }
+            catch ( Exception ex)
+            {
+
+                throw;
+            }
+
+
+            return tableSchemea;
+        }
+     
+
+        public  void InsertData(string TableName,List<string> Data)
+        {
+            string Query = "Insert  into "+TableName+" ";
+
+            var columns = GetTableSchema(TableName);
+            var firstchar = "(";
+            var lastchar = ")";
+            string columnNames = "";
+            string _data = "";
+            foreach (var item in columns)
+            {
+                columnNames +=item.Key+",";
+
+         
+               
+
+            }
+
+            foreach(var item in Data)
+            {
+                _data += "'"+item+"'" + ",";
+            }
+            
+            var ColumnNames=columnNames.Remove(columnNames.Length-1);
+           
+            var Datas=_data.Remove(_data.Length-1);
+           
+
+            Query =Query+firstchar+ ColumnNames +lastchar+" values " +firstchar+ Datas+lastchar;
+           
+            try
+            {
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+                SqlCommand command = new SqlCommand(Query, connection);
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(Query);
+                throw;
+            }
+
+
+        }
+        public bool Table( Dictionary<string,string> tableSechema,string tableName)
+        {
+
+            string Query = "CREATE TABLE "+tableName+" (";
+
+            string columnNames = "";
+
+            foreach(var item in tableSechema)
+            {
+                columnNames += ""+item.Key+""+" "+item.Value+" , ";
+            }
+
+            Query += columnNames + "date VARCHAR(64))";
+
+            
+            try
+            {
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+                SqlCommand command = new SqlCommand(Query, connection);
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
+            return true;
+        }
+
+       
+        public SqlDataReader DataReader { get; set; }
+
+        public bool doDbChange(string Query)
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+                SqlCommand command = new SqlCommand(Query, connection);
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                ErrorMessage = ex.Message;
+            }
+            return Status = true;
+        }
+
+        public List<string> getTable(string tableName)
+        {
+            List<string> TableData = new List<string>();
+
+            string Query = "Select"+" "+"*" +" " +"from  "+" "+tableName;
+            try
+            {
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+                SqlCommand sqlCommand = new SqlCommand(Query, connection);
+                DataReader = sqlCommand.ExecuteReader();
+                var count = DataReader.FieldCount;
+                int i = 0;
+                while (DataReader.Read())
+                {
+                    
+                   for(int j=0;j<count;j++)
+                    {
+
+                        Console.Write(DataReader.GetValue(j).ToString());
+                        TableData.Add(DataReader.GetValue(j).ToString());
+
+                    }
+
+
+
+
+                }
+                return TableData;
+
+            }
+            catch (Exception ex)
+            {
+
+                ErrorMessage = ex.Message;
+                Status = true;
+            }
+            return TableData;
+        }
+
+        public bool SendEmail(string toAddress, string subject, string message, string filePath)
+        {
+            try
+            {
+
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+                mail.From = new MailAddress("vishnukumarps2017@gmail.com");
+                mail.To.Add(toAddress);
+                mail.Subject = subject;
+                mail.Body = message;
+
+                if (filePath != null)
+                {
+                    System.Net.Mail.Attachment attachment;
+                    attachment = new System.Net.Mail.Attachment(filePath);
+                    mail.Attachments.Add(attachment);
+                }
+
+                SmtpServer.Port = 587;
+                SmtpServer.Credentials = new System.Net.NetworkCredential("vishnukumarps2017@gmail.com", "thriller1!");
+                SmtpServer.EnableSsl = true;
+
+                SmtpServer.Send(mail);
+                return (Status = true);
+            }
+            catch (Exception ex)
+            {
+
+                ErrorMessage = ex.Message;
+                return (Status = false);
+            }
+
+        }
+
+        //public bool SendSms(string toPhoneNumber, string msg)
+        //{
+        //    using (var web = new System.Net.WebClient())
+        //    {
+        //        try
+        //        {
+        //            string userName = "hostel123";
+        //            string userPassword = "1234567";
+        //            //string msgRecepient = "9656284838";
+        //            //string msgText = "This is a demo msg please ignore!";
+
+        //            string url2 = "http://198.24.149.4/API/pushsms.aspx?loginID=" + userName + "&password=" + userPassword + "&mobile=" + msgRecepient + "&text=" + msgText + "&senderid=CHPSMS&route_id=2&Unicode=0";
+        //            string result = web.DownloadString(url2);
+
+        //            if (result.Contains("OK"))
+        //            {
+        //                return Status = true;
+
+        //            }
+        //            else
+        //            {
+        //                return Status = false;
+        //            }
+        //        }
+        //        catch (Exception ex)
+        //        {
+
+        //            ErrorMessage = ex.Message;
+
+        //        }
+        //        return Status = true;
+        //    }
+
+
+
+        //}
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using TestAppLibrary;
+
+namespace DbGenerator
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+            HelperUtility utility = new HelperUtility();
+
+
+            //Dictionary<string, string> tableDetails = new Dictionary<string, string>();
+
+            //Console.WriteLine("\t\tCreate Table");
+
+            //Console.WriteLine("\n\n\nEnter Table Name");
+            //var table_Name = Console.ReadLine();
+            //Console.WriteLine("\n\t\t Enter Column num");
+            //int cn = Convert.ToInt32(Console.ReadLine());
+
+
+            //for (var i = 0; i < cn; i++)
+            //{
+            //    Console.WriteLine("\n\nEnter " + i + "Column name");
+            //    Console.WriteLine("Enter " + i + "DATATYPE(size)\n\n");
+            //    tableDetails.Add(Console.ReadLine(), Console.ReadLine());
+            //}
+
+
+            //tableDetails.Add("Name1", "VARCHAR(64)");
+            //tableDetails.Add("Age1", "VARCHAR(64)");
+            //tableDetails.Add("Name2", "VARCHAR(64)");
+            //tableDetails.Add("Name22", "VARCHAR(64)");
+            //tableDetails.Add("Name222", "VARCHAR(64)");
+            //tableDetails.Add("Name2222222222", "VARCHAR(64)");
+            //tableDetails.Add("Age12", "VARCHAR(64)");
+            //tableDetails.Add("Age122", "VARCHAR(64)");
+            //tableDetails.Add("Age2121", "VARCHAR(64)");
+            //tableDetails.Add("Age12121212", "VARCHAR(64)");
+
+            //utility.Table(tableDetails, table_Name);
+
+
+            Console.WriteLine("\t\t All Tables\n");
+          
+            var tables=utility.GetAllTableNames();
+
+
+            foreach(var item in tables)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("\n\n\n\t Select a Table");
+
+            var tableName = Console.ReadLine();
+            var table=utility.GetTableSchema(tableName);
+            foreach (var item in table)
+            {
+                Console.WriteLine(item.Key+" "+item.Value);
+            }
+
+            Console.WriteLine("\n\n\n\t\t\t\t Insert data to "+tableName);
+
+            List<string> data = new List<string>();
+            foreach (var item in table.Keys)
+            {
+                Console.WriteLine("Enter "+item+"\n\n");
+                data.Add(Console.ReadLine());
+            }
+
+            utility.InsertData(tableName,data);
+            Console.WriteLine("Inserted");
+            Console.WriteLine("Enter the  table Name to See the data");
+            var tbl = Console.ReadLine();
+            
+            var Tdata=utility.getTable(tbl);
+            foreach(var item in Tdata)
+            {
+                Console.Write(item+"   ");
+            }
+           
+
+            Console.ReadKey();
+            Console.ReadKey();
+        }
+    }
+}
